9b5fcab8372cb0246d2fea359976bcb65552c8e4
oogab <oogab@naver.com>
2022-05-01 13:05:41 +0900

    Get My Own Nweets

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,13 +1,31 @@
-import React from "react";
-import { authService } from "fbase";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect } from "react";
+import { authService, dbService } from "fbase";
+import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
+// import { useNavigate } from "react-router-dom";
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
   // const navigate = useNavigate()
   const onLogOutClick = () => {
     authService.signOut()
     // navigate("/")
   }
+
+  const getMyNweets = async () => {
+    const q = query(
+      collection(dbService, "nweets"),
+      where("creatorId", "==", userObj.uid),
+      orderBy("createdAt", "desc")
+    )
+    const querySnapshot = await getDocs(q)
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, "=>", doc.data())
+    })
+  }
+
+  useEffect(() => {
+    getMyNweets()
+  }, [])
+
   return (
     <>
         <button onClick={onLogOutClick}>Log Out</button>
